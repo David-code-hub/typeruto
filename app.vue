ec0cea1f2029b39b038bf3dc1523c02a0c0f4439
David <david@rlabs.org>
2024-12-15 22:35:41 +0200

    add: keyboard components

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="max-w-screen-sm mx-auto flex flex-col my-auto">
+    <h1 class="font-bold text-3xl">Typeroo!</h1>
+    <Keyboard />
   </div>
 </template>
---
+++ components/keyboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div>
+      <p>{{ quote }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const quote = "One day at a time";
+</script>
