c3b6bd7401a0021d8b5534d586f15094ba4f427c
David <david@rlabs.org>
2024-12-15 23:20:19 +0200

    add: update text color on keyup

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="max-w-screen-sm mx-auto flex flex-col my-auto">
-    <h1 class="font-bold text-3xl">Typeroo!</h1>
-    <Keyboard />
+  <div class="mt-10">
+    <div class="max-w-screen-sm mx-auto my-auto flex flex-col">
+      <div class="mb-10">
+        <h1 class="font-bold text-3xl">Typeroo!</h1>
+      </div>
+      <Keyboard />
+    </div>
   </div>
 </template>
