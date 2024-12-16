ed36127efeea10af398ccfa19d578aa00274e10d
David <david@rlabs.org>
2024-12-16 21:37:53 +0200

    add: reload option,text display

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div class="mt-10">
     <div class="max-w-screen-sm mx-auto my-auto flex flex-col">
       <div class="mb-10">
-        <h1 class="font-bold text-3xl">Typeroo!</h1>
+        <h1 class="font-bold text-3xl">Typeru</h1>
       </div>
       <Keyboard />
     </div>
--- components/keyboard.vue
+++ components/keyboard.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
     <div>
+      <div class="flex justify-between mb-7 items-center">
+        <div>wpd:60 accuracy: 89%</div>
+        <div>
+          <button
+            @click="getNextQuote"
+            class="duration-300 hover:bg-gray-100 py-1 px-2 rounded-lg flex gap-1 items-center"
+          >
+            <Icon name="simple-line-icons:reload" class="size-5" />
+            Reload
+          </button>
+        </div>
+      </div>
       <div v-if="loading" class="w-full">
         <div class="text-center">
           <Icon name="svg-spinners:180-ring-with-bg" class="size-6" />
           <p>Fetching quote...</p>
         </div>
       </div>
-      <div class="font-mono text-2xl" v-else>
+
+      <div class="font-mono text-3xl break-words" v-else>
         <span
-          v-for="(letter, index) in quote.replace(' ', '_').split('')"
+          class="text-gray-400"
+          v-for="(letter, index) in quote"
           :key="letter + index"
           :id="letter + index"
         >
@@ -18,6 +32,7 @@
       </div>
     </div>
   </div>
+  <div class="w-[3px] bg-black h-7 animate-pulse"></div>
 </template>
 
 <script setup lang="ts">
@@ -26,10 +41,10 @@ const loading = ref(false);
 const index = ref(0);
 
 function checkTyping(event: KeyboardEvent) {
-  const splitQuoteByIndex = quote.value.split("")[index.value];
+  const splitQuoteByIndex = quote.value[index.value];
   const spanElement = document.getElementById(splitQuoteByIndex + index.value);
 
-  if (splitQuoteByIndex === event.key) {
+  if (splitQuoteByIndex === "_" ? " " : splitQuoteByIndex === event.key) {
     spanElement?.classList.remove("text-red-700");
     spanElement?.classList.add("text-green-700");
     index.value += 1;
@@ -43,8 +58,7 @@ async function getNextQuote() {
   try {
     loading.value = true;
     const response = await fetch("https://dummyjson.com/quotes/random");
-    quote.value = (await response.json()).quote;
-    console.log("quote response :", quote.value);
+    quote.value = (await response.json()).quote.replace(/ /g, "_").split("");
   } catch (error) {
     console.error("Error while fetching quote :", error);
   } finally {
