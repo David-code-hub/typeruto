471001b93148e628370b7e33e8ab38dc6d6350c6
David <david@rlabs.org>
2024-12-23 19:20:05 +0200

    add: naruto quotes,remove: api endpoint

---
+++ composables/narutoQuotesData.ts
@@ -0,0 +1,219 @@
+export const narutoQuotes = [
+  {
+    quote: "Hard work is worthless for those that don’t believe in themselves.",
+    character: "Naruto Uzumaki",
+  },
+  {
+    quote:
+      "When you give up, your dreams and everything else are gone forever.",
+    character: "Naruto Uzumaki",
+  },
+  {
+    quote:
+      "A person becomes strong when they have someone they want to protect.",
+    character: "Haku",
+  },
+  {
+    quote: "The pain of being alone is completely out of this world, isn’t it?",
+    character: "Naruto Uzumaki",
+  },
+  {
+    quote:
+      "People's lives don’t end when they die; they end when they give up.",
+    character: "Naruto Uzumaki",
+  },
+  {
+    quote:
+      "When people are protecting something truly special, they become as strong as they can be.",
+    character: "Naruto Uzumaki",
+  },
+  {
+    quote:
+      "The moment people stop chasing their dreams is the moment they truly die.",
+    character: "Naruto Uzumaki",
+  },
+  {
+    quote:
+      "If you don’t like your destiny, don’t accept it. Have the courage to change it.",
+    character: "Naruto Uzumaki",
+  },
+  {
+    quote: "While I walk, I’ll always be running toward my true goal in life.",
+    character: "Naruto Uzumaki",
+  },
+  {
+    quote:
+      "I care more about others than myself, and I won’t let anyone hurt them.",
+    character: "Naruto Uzumaki",
+  },
+  {
+    quote:
+      "Power is not will; it is the phenomenon of physically making things happen.",
+    character: "Madara Uchiha",
+  },
+  {
+    quote: "A person grows up when they’re able to overcome hardships.",
+    character: "Jiraiya",
+  },
+  {
+    quote:
+      "Failing doesn’t give you a reason to give up as long as you believe.",
+    character: "Naruto Uzumaki",
+  },
+  {
+    quote: "If love is just a word, why does it hurt so much when we lose it?",
+    character: "Gaara",
+  },
+  {
+    quote:
+      "Those who forgive themselves, and accept their true nature, they are the strong ones.",
+    character: "Itachi Uchiha",
+  },
+  {
+    quote:
+      "A real ninja is one who endures no matter what gets thrown at them.",
+    character: "Jiraiya",
+  },
+  {
+    quote:
+      "In the ninja world, those who break the rules are scum. But those who abandon their friends are worse than scum.",
+    character: "Kakashi Hatake",
+  },
+  {
+    quote: "When a man learns to love, he must bear the risk of hatred.",
+    character: "Madara Uchiha",
+  },
+  {
+    quote: "Pain is the way we grow stronger, and our scars are our teachers.",
+    character: "Naruto Uzumaki",
+  },
+  {
+    quote: "Some people want power and are willing to pay any price for it.",
+    character: "Orochimaru",
+  },
+  {
+    quote:
+      "You don’t have to forgive me, but no matter what happens, I will always love you.",
+    character: "Itachi Uchiha",
+  },
+  {
+    quote: "Because you fail, you have the strength to rise again.",
+    character: "Hinata Hyuga",
+  },
+  {
+    quote: "True power is not about taking a life, but about saving it.",
+    character: "Hiruzen Sarutobi",
+  },
+  {
+    quote: "The people who are precious to you, do you let them die so easily?",
+    character: "Nagato (Pain)",
+  },
+  {
+    quote:
+      "Rejection is a part of any man’s life. If you can’t accept and move past rejection, you’re not worthy of a man at all.",
+    character: "Jiraiya",
+  },
+  {
+    quote: "Man seeks peace, yet at the same time yearning for war.",
+    character: "Madara Uchiha",
+  },
+  { quote: "The weaker you are, the louder you bark.", character: "Tsunade" },
+  {
+    quote:
+      "There is no such thing as hope. The concept of hope is nothing more than giving up.",
+    character: "Obito Uchiha",
+  },
+  {
+    quote:
+      "My name is Sasuke Uchiha. I hate a lot of things, and I don’t particularly like anything.",
+    character: "Sasuke Uchiha",
+  },
+  {
+    quote:
+      "When you feel the worst in life, take a breath and move forward. That’s the ninja way.",
+    character: "Naruto Uzumaki",
+  },
+  { quote: "People cannot win against their loneliness.", character: "Gaara" },
+  {
+    quote:
+      "Love breeds sacrifice, which in turn breeds hatred. Then you can know pain.",
+    character: "Pain (Nagato)",
+  },
+  {
+    quote:
+      "Fear is not evil. It tells you what your weakness is. And once you know your weakness, you can become stronger.",
+    character: "Gildarts Clive",
+  },
+  {
+    quote: "As long as you keep fighting, there’s a chance you’ll survive.",
+    character: "Kabuto Yakushi",
+  },
+  {
+    quote: "The world isn’t beautiful. Therefore, it is.",
+    character: "Kakashi Hatake",
+  },
+  {
+    quote: "I will become a great Hokage someday!",
+    character: "Naruto Uzumaki",
+  },
+  {
+    quote:
+      "The world is full of pain and suffering. That’s why we need to believe in each other.",
+    character: "Nagato (Pain)",
+  },
+  {
+    quote:
+      "The true measure of a shinobi is not how he lives, but how he dies.",
+    character: "Jiraiya",
+  },
+  {
+    quote:
+      "I’ve been here long enough to know that the world isn’t a fair place.",
+    character: "Shikamaru Nara",
+  },
+  {
+    quote: "The true worth of a ninja is not how they live, but how they die.",
+    character: "Itachi Uchiha",
+  },
+  {
+    quote: "Even the strongest of opponents always has a weakness.",
+    character: "Itachi Uchiha",
+  },
+  {
+    quote:
+      "Happiness is not something ready-made. It comes from your own actions.",
+    character: "Naruto Uzumaki",
+  },
+  {
+    quote: "I’d rather be a fool than stay the same.",
+    character: "Naruto Uzumaki",
+  },
+  {
+    quote:
+      "Growth occurs when one goes beyond one’s limits. Realizing that is also part of training.",
+    character: "Itachi Uchiha",
+  },
+  {
+    quote: "I am the one who will stand at the top.",
+    character: "Sasuke Uchiha",
+  },
+  {
+    quote:
+      "Forgetting is like a wound. The wound may heal, but it has already left a scar.",
+    character: "Naruto Uzumaki",
+  },
+  {
+    quote: "A snake that sloughs its skin will never be caught by a hunter.",
+    character: "Orochimaru",
+  },
+  {
+    quote:
+      "In society, those who don’t have many abilities tend to complain more.",
+    character: "Neji Hyuga",
+  },
+  {
+    quote:
+      "I’m telling you this because you don’t get it. You think you get it, which isn’t the same as actually getting it. Get it?",
+    character: "Kakashi Hatake",
+  },
+];
